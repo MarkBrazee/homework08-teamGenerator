7dabc2824f022cbc9f71652c7841608b2de60234
Mark Brazee <m.brazee@comcast.net>
2020-10-19 17:05:18 -0600

    commit w-o modules

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// Adding info for second push
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
